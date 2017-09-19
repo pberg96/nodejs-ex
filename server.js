4b963d8ef98af698c4389c3a4ba99abc2fccf63c
pberg96 <pberg@live.unc.edu>
2017-09-19 16:07:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-var http = require('http');
-var express = require('express');
-var app = express();
-
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
-
-http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+const express = require('express')
+const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
